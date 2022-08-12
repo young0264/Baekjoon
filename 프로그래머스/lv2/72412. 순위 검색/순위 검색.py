1055847273b5c6081c4080abf8918e35d3130e25
young0264 <96028167+young0264@users.noreply.github.com>
2022-08-12 10:00:48 +0900

    [level 2] Title: 순위 검색, Time: 518.90 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -5,7 +5,7 @@ def solution(info, query):
     answer = []
     dic = defaultdict(list)
 
-    for inf in info:
+    for inf in info:    #공백으로 구분되어 있어
         inf = inf.split(' ')
         info_key = inf[0:-1]
         info_value = int(inf[-1])
@@ -14,33 +14,38 @@ def solution(info, query):
             for j in combi:
                 res = ''.join(j)
                 dic[res].append(info_value)
+
     for key in dic:
         dic[key].sort()
-    for q in query:
+
+    ##== 쿼리 갯수만큼 ==##
+    for q in query: #마지막 음식과 점수는 and가 아니라 공백으로 구분되어 있어
         q = q.split(' ')
         q_key = q[:-1]
         q_value = int(q[-1])
 
+        ##==O(N^2)==##
         for i in range(3):
-            q_key.remove('and')
-
+            q_key.remove('and') #한번 삭제할때마다 O(N) 앞에있는 것부터 한번에 하나씪 삭제하게됨
+        ##==O(N^2)==##
         while '-' in q_key:
             q_key.remove('-')
         query_key = ''.join(q_key)
-
-
+        ##==info의 경우의수==##
         if query_key in dic:
             scoreList = dic[query_key]
 
+        ##==이분탐색==##
             if len(scoreList) > 0:
-                left, right = 0, len(scoreList)
-                while left < right:
-                    mid = (left + right) // 2
-                    if scoreList[mid] >= q_value:
-                        right = mid
-                    else:
-                        left = mid + 1
-                answer.append(len(scoreList) - left)
+                answer.append(len(scoreList) - bisect_left(scoreList,q_value))
+                # left, right = 0, len(scoreList)
+                # while left < right:
+                #     mid = (left + right) // 2
+                #     if scoreList[mid] >= q_value:
+                #         right = mid
+                #     else:
+                #         left = mid + 1
+                # answer.append(len(scoreList) - left)
         else:
             answer.append(0)
 
