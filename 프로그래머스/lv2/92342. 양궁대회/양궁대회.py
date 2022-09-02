64991ee376ada2a6aad60d5f803ab17734ef7939
young0264 <96028167+young0264@users.noreply.github.com>
2022-09-02 23:56:37 +0900

    [level 2] Title: 양궁대회, Time: 460.54 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -0,0 +1,48 @@
+from itertools import combinations_with_replacement
+def solution(n, info):
+    answer = []
+    cha_value = 0
+    arr = [i for i in range(11)]
+    for  lion_info in combinations_with_replacement(arr,n):
+        lion_arr = [0]*11
+        lion_cnt , apeach_cnt = 0,0
+        for i in lion_info:
+            lion_arr[i] += 1
+
+        for i in range(11): #[0 2 2 0 1 0 0 0 0 0 0 0 ]
+            if lion_arr[i] > info[i]:
+                lion_cnt += 10-i
+            elif lion_arr[i] == info[i] == 0:
+                continue
+            elif lion_arr[i] <= info[i]:
+                apeach_cnt += 10-i
+        flag = 0
+        if lion_cnt > apeach_cnt:
+            if cha_value < lion_cnt-apeach_cnt:
+                cha_value = lion_cnt-apeach_cnt
+                answer = lion_arr
+            elif cha_value == lion_cnt-apeach_cnt   :
+                for i in range(10,-1,-1):
+                    if lion_arr[i] == answer[i]==0:
+                        continue
+                    elif lion_arr[i] > answer[i]:
+                        answer = lion_arr
+                        break
+                    else:
+                        break
+
+
+    if not answer :
+        answer.append(-1)
+    return answer
+
+
+
+
+
+
+
+
+
+
+
