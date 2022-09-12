713012a704904b8893cc2a1e1745f3b0cb4d7c08
young0264 <96028167+young0264@users.noreply.github.com>
2022-09-12 22:58:10 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -0,0 +1,44 @@
+import sys
+def dfs(x,y,dir,res):
+    global graph,answer
+    value = 100
+    if x==r-1 and y==c-1:
+        # for i in visited:
+        #     print(*i)
+        # print()
+        # print("ans",answer)
+        answer = min(answer,res)
+        return
+    for i in range(4):#동남서북
+        nx,ny = x+dxs[i],y+dys[i]
+        if in_range(nx,ny) and graph[nx][ny] != 1:
+
+            if abs(dir-i) == 1 or abs(dir-i) == 3:
+                value = 600
+            else:
+                value = 100
+            if visited[nx][ny] :
+                if visited[nx][ny] > visited[x][y] + value:
+                    visited[nx][ny] = visited[x][y] + value
+                    dfs(nx,ny,i,res+value)
+            else:
+                visited[nx][ny] = visited[x][y] + value
+                dfs(nx,ny,i,res+value)
+def in_range(x,y):
+    global r,c
+    return 0<=x<r and 0<=y<c
+
+def solution(board):
+    global r,c,dxs,dys,visited,graph,answer
+    graph = board
+    r,c = len(board) , len(board[0])
+    dxs,dys = [0,1,0,-1],[1,0,-1,0]#동남서북
+    answer = sys.maxsize
+    visited = [[0]*c for _ in range(r)]
+    visited[0][0] = 100
+    dfs(0,0,1,0)
+    visited = [[0]*c for _ in range(r)]
+    dfs(0,0,2,0)
+
+    # print(answer)
+    return answer
