dddf016ae787a4fe3efbf6922a3400714080989f
young0264 <96028167+young0264@users.noreply.github.com>
2022-09-23 16:44:53 +0900

    [level 2] Title: 양궁대회, Time: 609.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,5 +1,5 @@
 from itertools import combinations_with_replacement
-def solution(n, info):
+def solution(n, apeach_arr):
     answer = []
     cha_value = 0
     arr = [i for i in range(11)]
@@ -10,12 +10,13 @@ def solution(n, info):
             lion_arr[i] += 1
 
         for i in range(11): #[0 2 2 0 1 0 0 0 0 0 0 0 ]
-            if lion_arr[i] > info[i]:
+            if lion_arr[i] > apeach_arr[i]:
                 lion_cnt += 10-i
-            elif lion_arr[i] == info[i] == 0:
+            elif lion_arr[i] == apeach_arr[i] == 0:
                 continue
-            elif lion_arr[i] <= info[i]:
+            elif lion_arr[i] <= apeach_arr[i]:
                 apeach_cnt += 10-i
+                
         flag = 0
         if lion_cnt > apeach_cnt:
             if cha_value < lion_cnt-apeach_cnt:
@@ -31,9 +32,9 @@ def solution(n, info):
                     else:
                         break
 
-
     if not answer :
         answer.append(-1)
+        
     return answer
 
 
