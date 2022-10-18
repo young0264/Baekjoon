a7002014e0428423679d59ac9c004ac35ad13318
영이 <96028167+young0264@users.noreply.github.com>
2022-10-18 21:53:15 +0900

    [Silver I] Title: 1로 만들기 2, Time: 576 ms, Memory: 102628 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -24,28 +24,22 @@ def bfs():
             answer_arr.reverse()
             print(visited3[1][0]-1)
             print(*answer_arr)
-
-
             return
-        for i in range(3):
-            if i==0:
-                if not dx%3:
-                    nx = dx//3
-                    if 0<nx<=n and not visited3[nx]:
-                        visited3[nx] = [visited3[dx][0]+1, dx]
-                        que.append(nx)
 
-            elif i==1:
-                if not dx%2:
-                    nx = dx//2
-                    if 0<nx<=n and not visited3[nx]:
-                        visited3[nx] = [visited3[dx][0]+1, dx]
-                        que.append(nx)
-            else:
-                nx = dx-1
-                if 0 < nx <= n and not visited3[nx]:
-                    visited3[nx] = [visited3[dx][0] + 1, dx]
-                    que.append(nx)
+        if not dx%3:
+            nx = dx//3
+            if 0<nx<=n and not visited3[nx]:
+                visited3[nx] = [visited3[dx][0]+1, dx]
+                que.append(nx)
+        if not dx%2:
+            nx = dx//2
+            if 0<nx<=n and not visited3[nx]:
+                visited3[nx] = [visited3[dx][0]+1, dx]
+                que.append(nx)
+        nx = dx-1
+        if 0 < nx <= n and not visited3[nx]:
+            visited3[nx] = [visited3[dx][0] + 1, dx]
+            que.append(nx)
 
 bfs()
 
