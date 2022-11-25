2a939c7023de8853ff3749141c1ac9416c1fde4a
영이 <96028167+young0264@users.noreply.github.com>
2022-11-25 21:48:08 +0900

    [Silver II] Title: 안녕, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -0,0 +1,17 @@
+n = int(input())
+HP = [0] + list(map(int, input().split()))  # 잃는 체력
+Happy = [0] + list(map(int, input().split()))  # 얻는 기쁨
+
+dp = [[0] * (101) for _ in range(n + 1)]
+answer = 0
+for i in range(1, n + 1):
+    for j in range(1, 101):
+        if (100 - j) - HP[i] >= 0:
+            dp[i][100 - j] = max(dp[i - 1][100 - j - HP[i]] + Happy[i], dp[i - 1][100 - j])
+        else:
+            dp[i][100 - j] = dp[i - 1][100 - j]
+        answer = max(answer, dp[i][100 - j])
+
+
+# print(dp[-1][-2])
+print(answer)
