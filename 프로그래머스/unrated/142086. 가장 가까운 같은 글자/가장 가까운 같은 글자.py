593914e93a4847901e4ebf0a518d336f09d22b98
영이 <96028167+young0264@users.noreply.github.com>
2022-12-12 11:43:05 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.63 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = []
+    dic = dict()
+    for i in range(len(s)):
+        if s[i] not in dic:
+            answer.append(-1)
+            dic[s[i]] = i
+        else:
+            answer.append(i-dic[s[i]])
+            dic[s[i]] = i
+            
+    
+    
+    return answer
