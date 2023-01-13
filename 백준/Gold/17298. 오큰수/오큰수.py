468ca4d5d3182be04c72e40286c823bb812de1c2
영이 <96028167+young0264@users.noreply.github.com>
2023-01-13 23:19:25 +0900

    [Gold IV] Title: 오큰수, Time: 744 ms, Memory: 261508 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,3 +1,9 @@
+'''
+뒤에 들어올 수 arr[i]가 arr[stack[-1]]보다 크면 stack[-1]=idx에 해당하는 위치의 answer[idx]에 현재값 arr[i]를 저장
+
+앞서 입력된 수보다는 작은수들이 들어오기 때문에 현재의 arr[i]값을 기준으로 while stack을 통한 answer 값을 보장할 수 있다.
+'''
+
 n = int(input())
 arr = list(map(int,input().split()))
 stack = [0]
@@ -13,3 +19,7 @@ for i in range(1,n):
     stack.append(i)
 
 print(*answer)
+
+
+
+
